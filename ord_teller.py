5137bbc7871e8dce35af3778ebb05ead6b7c4431
henrba2406 <henrba2406@icloud.com>
2020-06-02 11:00:22 +0200

    added vowels

--- ord_teller.py
+++ ord_teller.py
@@ -1,19 +1,28 @@
+import mathplotlib.pyplot as plt
 f = open("input.log","r") #åpner fila vi skal telle vokaler i teksten med
-
-f_out = open("output.txt","w") #åpne utskriftsfil fo å lage data
-
+f_out  = open("output.txt","w") #åpne utskriftsfil fo å lage data
 file_content = f.read() #lagre innhold i fila til file_content
 
-count = 0
+vokal_array = ['a', 'e', 'i', 'o', 'u', 'y']
+n = len(vokal_array)
+telle_array = [0]*n
 
 for i in file_content:
-    if (i == 'a') or (i == 'A') or (i == 'e') or (i == 'E') or (i == 'i') or (i == 'I') or (i == 'o') or (i == 'O') or (i == 'u') or (i == 'U') or (i == 'y') or (i == 'Y'):
-        count = count + 1
+    index = 0
+    for k in vokal_array:
+        if (i.lower() == k):
+            telle_array[index] = telle_array[index] + 1
+        index = index + 1
 
-print(count)
-
-f_out.write("A or a: ") #skriv til output.tex om
-f_out.write(str(count)) #hvor mange store og små A'er vi fant
+index = 0
+for k in vokal_array:
+    f_out.write(k)
+    f_out.write(": ")
+    f_out.write(str(telle_array[index]))
+    f_out.write("\n")
+    index = index + 1
 
 f.close() #alltid lukk fila etter bruk 
 f_out.close()
+
+plt.plot(vokal_array,telle_array)
